d7e6f578c54d28f551adf2313477a1274209a8af
feaaw <164832933+feaaw@users.noreply.github.com>
2026-01-08 20:42:23 +0900

    Create Day 07.py

---
+++ 30days_of_code_Python/Day 07.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    reversed_array = []
+    for i in range(n):
+        reversed_array.append(arr[n - i - 1])
+        
+    output_string = ''
+    for i in range(len(reversed_array)):
+        output_string += str(reversed_array[i]) +  ' '
+    print(output_string)
