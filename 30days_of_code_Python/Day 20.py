0512a3898dc318144d88e38a0b17aa7c0749cf2a
feaaw <164832933+feaaw@users.noreply.github.com>
2026-01-21 22:45:05 +0900

    Add bubble sort implementation in Day 20.py
    Implement bubble sort algorithm to sort an array and count swaps.

---
+++ 30days_of_code_Python/Day 20.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    # Write your code here
+    total_swaps = 0
+    for i in range(n):
+        swaps = 0
+        for j in range(n - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swaps += 1
+        total_swaps += swaps
+        if swaps == 0:
+            break
+            
+    print("Array is sorted in", total_swaps, "swaps.")
+    print("First Element:", a[0])
+    print("Last Element:", a[-1])
